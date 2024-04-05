ed1e88ab71e588622489ca128c15b38f77b9b2bd
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-04-05 10:19:53 +0200

    changed <h3> to <h4>

--- Module_7_StudentNo_Classcode_Group_Name-Surname_JSL07-main/Module_7_StudentNo_Classcode_Group_Name-Surname_JSL07-main/index.js
+++ Module_7_StudentNo_Classcode_Group_Name-Surname_JSL07-main/Module_7_StudentNo_Classcode_Group_Name-Surname_JSL07-main/index.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
       <p>This is to certify that</p>
       <h3>${studentName}</h3>
       <p>has almost completed</p>
-      <h4>${courseName}<h3>
+      <h4>${courseName}<h4>
       <p>with legendary perserverance and world-class bad-assery for never giving up🏆.</p>
       <img src="./logo.png" alt="Seal" style="margin-top: 20px; max-height: 100px;">
       <p>${personalMessage}</p>
